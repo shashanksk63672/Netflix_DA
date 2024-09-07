04067739680e44acfa03f51096cec72a848dae5a
Shashank sharma <133596454+shashanksk63672@users.noreply.github.com>
2024-09-08 02:00:40 +0530

    Add files via upload

---
+++ netflix.sql
@@ -0,0 +1,19 @@
+
+create table netflix
+(
+show_id	varchar(6),
+	type	varchar(10),
+	title	varchar(150),
+	director varchar(250),	
+	casts	varchar(1000),
+	country	varchar(150),
+	date_added	varchar(50),
+	release_year	int,
+	rating	varchar(10),
+	duration varchar(10),
+	listed_in	varchar(100),
+	description varchar(250)
+
+)
+
+select * from netflix
